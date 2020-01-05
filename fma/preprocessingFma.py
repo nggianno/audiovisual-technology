ae1efb8fea3c48709247399e56e180096c55a92e
Odysseas <podysseas@auth.gr>
2020-01-05 18:49:57 +0200

    super tidy

--- fma/preprocessingFma.py
+++ fma/preprocessingFma.py
@@ -12,68 +12,39 @@ def preprocess_csv_files(tracks,echonest,features):
      return tracks,echonest,features
 
 def make_tracks_dataset(data):
+    
      clean_tracks = data[['track_id',"genre_top"]] 
      clean_tracks.drop(clean_tracks.index[0], inplace=True)
-<<<<<<< HEAD
-     # !!!!!!!!!
-     # Do you want merge with echonest? Uncomment the next one 
+	 
+	 # Do you want merge with features? Uncomment the next line
+     clean_tracks = clean_tracks.merge(features,on="track_id",how="inner")
+     # Here we have at least 1000 songs "Classical" if you choose to merge
+     frequencies = clean_tracks['genre_top'].value_counts()
+     print("1st step :\n",frequencies) 
+     # 
+     # Do you want merge with echonest? Uncomment the next line 
      # clean_tracks = clean_tracks.merge(echonest,on="track_id",how="inner")
-     #
-=======
-     print(clean_tracks.shape)
-     # clean_na_tracks : tracks with no NAN in column "genre_top"
-     #clean_na_tracks = clean_tracks.dropna()
-     # songs of each genre
->>>>>>> 71570e5d8a30d7b8c0a0d752fed04ab066529a82
+     # 
      frequencies = clean_tracks['genre_top'].value_counts()
-     print(frequencies) 
-     # !!!!!!!!!
-     # Do you want merge with features? Uncomment the next one 
-     # clean_tracks = clean_tracks.merge(features,on="track_id",how="inner")
-     #
-     data_final = data.merge(features, on='track_id', how='inner')
-     frequencies = clean_tracks['genre_top'].value_counts()
-     print(frequencies) 
+     print("2nd step :\n",frequencies) 
      
-     # select genres and how many songs each genre
-     data = pd.DataFrame()
-     selected_genres = ['Pop','Rock','Hip-Hop','Classical']
      clean_tracks= clean_tracks.sample(frac=1).reset_index(drop=True)
-     for i in selected_genres:
-          songs = clean_tracks.loc[clean_tracks['genre_top'] == i]
-          songs = songs.head(1000)
-<<<<<<< HEAD
-          data = data.append(songs)
-     data.reset_index(inplace=True)
-     data.drop(columns='index', inplace=True)
-     print("\ndata_final:\n",data_final['genre_top'].value_counts())
+     # desired features can be both from features df and echonest df
+     # if you choose to merge
+     desired_features='track_id','genre_top',"spectral_bandwidth.2",'zcr.2','spectral_centroid.2','spectral_rolloff.2'
+     dataset= clean_tracks.loc[:,desired_features]
+     # Uncomment the next line if you choose to merge
+     dataset = dataset.join(clean_tracks[c[36:56]]) 
+	 #!!!!!!!!!! here index 36:56 are the mffc mean in features
      
-     # keep only the feautures that we want : column names for both echonest or features 
-     # if they are merged
-     desired_features='track_id','genre_top',"spectral_bandwidth.2",'zcr.2','spectral_centroid.2', 'spectral_rolloff.2'
-     dataset= data_final.loc[:,desired_features]
-     
-     max_tracks_each_genre = 800
+     #  selected_genres :
+     # 'Pop','Rock','Hip-Hop','Classical'
+     max_tracks_each_genre = 1000 # choose how many songs you will 
      classical = dataset[dataset['genre_top'] == 'Classical'].head(max_tracks_each_genre)
      rock = dataset[dataset['genre_top'] == 'Rock'].head(max_tracks_each_genre)
      hip_hop = dataset[dataset['genre_top'] == 'Hip-Hop'].head(max_tracks_each_genre)
      pop = dataset[dataset['genre_top'] == 'Pop'].head(max_tracks_each_genre)
-     
-     # combine each to our final dataset 
-=======
-          tracks = tracks.append(songs)
-
-     tracks.reset_index(inplace=True)
-     tracks.drop(columns='index', inplace=True)
-     tracks_final = tracks.merge(features, on='track_id', how='inner')
-     tracks = pd.DataFrame(tracks_final)
-     dataset = tracks[['track_id', 'genre_top', 'zcr.2','rmse.2','spectral_centroid.2','spectral_bandwidth.2','spectral_rolloff.2']]
-     classical = dataset[dataset['genre_top'] == 'Classical'].head(400)
-     rock = dataset[dataset['genre_top'] == 'Rock'].head(400)
-     hip_hop = dataset[dataset['genre_top'] == 'Hip-Hop'].head(400)
-     pop = dataset[dataset['genre_top'] == 'Pop'].head(400)
-
->>>>>>> 71570e5d8a30d7b8c0a0d752fed04ab066529a82
+     # append each to our final dataset 
      final_dataset = pd.DataFrame()
      final_dataset = final_dataset.append([classical,hip_hop,rock,pop])
 
@@ -100,20 +71,25 @@ if __name__=='__main__':
      features = pd.read_csv(FEATURE_PATH)
      echonest = pd.read_csv(ECH0NEST_PATH,header = 2)
 
-     print(features.columns)
      (tracks,echonest,features) = preprocess_csv_files(tracks,echonest,features)
      """make a dataset of 400 * 4 categories(POP,ROCK,CLASSICAL,HIP-HOP)"""
-   
+     # !!!!!!!!
+     # just to find where are the mfcc mean in features df 
+     p = pd.DataFrame()
+     for column in features.columns:
+         temp = features[column].iloc[0]
+         #print(temp)
+         if (temp == "mean"):
+             p = p.join(features[column])
+  
+     c = pd.array(p.columns)
+     # 
      final = make_tracks_dataset(tracks)
      
      final.rename(columns={"zcr.2": "zcr"}, inplace=True)
-     final.rename(columns={"rmse.2": "rmse"}, inplace=True)
      final.rename(columns={"spectral_rolloff.2": "spectral_rollof"}, inplace=True)
      final.rename(columns={"spectral_centroid.2": "spectral_centroid"}, inplace=True)
-     final.rename(columns={"spectral_bandwidth.2": "spectral_bandwidth"}, inplace=True)
-
      print(final.head())
-
-     #extract_to_csv(final)
+     extract_to_csv(final)
 
 
